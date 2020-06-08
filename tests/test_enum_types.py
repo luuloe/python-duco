1767e6a884fe60178c9386d34fcbb260731621fe
luuloe <luukloeffen@hotmail.com>
2020-06-08 19:20:32 +0200

    small commit to trigger codeacy

--- tests/test_enum_types.py
+++ tests/test_enum_types.py
@@ -7,8 +7,8 @@ from duco.enum_types import (ModuleType)
 
 class TestModuleType(unittest.TestCase):
     def test_supported(self):
-        for x in range(DUCO_MODULE_TYPE_MASTER, 10):
-            self.assertTrue(ModuleType.supported(x), "msg")
+        for idx in range(DUCO_MODULE_TYPE_MASTER, 10):
+            self.assertTrue(ModuleType.supported(idx), "msg")
 
         self.assertFalse(ModuleType.supported(DUCO_MODULE_TYPE_MASTER-1), "")
         self.assertFalse(ModuleType.supported(
